f58e4ec8a969df38b4b94d1e55fe3c980a047f1a
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-10-08 17:38:36 -0500

    Prueba 2

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
 	var base, altura float32
-
-	fmt.Printf("Dime cuanto mide la base del triangulo: ")
 	fmt.Scanf("%f", &base)
-	fmt.Printf("Dime cuanto mide la altura del triangulo: ")
 	fmt.Scanf("%f", &altura)
-	total := (base*altura)/2
-	fmt.Println("El area del triangulo es: ", total)
+	area := (base*altura)/2
+	fmt.Println(area)
 }
