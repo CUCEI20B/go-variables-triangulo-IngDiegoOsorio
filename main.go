851c587f1ad5931c8f8070054d120add2d1627ec
repl.it user <replituser@example.com>
2020-10-08 20:49:33 +0000

    Se agrega codigo para saber la base del triangulo

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var base, altura float32
+
+	fmt.Printf("Dime cuanto mide la base del triangulo: ")
+	fmt.Scanf("%f", &base)
+	fmt.Printf("Dime cuanto mide la altura del triangulo: ")
+	fmt.Scanf("%f", &altura)
+	total := (base*altura)/2
+	fmt.Println("El area del triangulo es: ", total)
 }
